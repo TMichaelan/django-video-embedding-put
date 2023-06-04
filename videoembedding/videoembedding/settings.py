5d5129028dbcccb5dc41b285997092bf178c4517
TMichael <bigapplematthias@gmail.com>
2023-06-04 15:30:29 +0200

    init new

--- videoembedding/videoembedding/settings.py
+++ videoembedding/videoembedding/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$ia*b@0*&m$ea7ag%j0!8ysh+xzt1xrq&^dcxbzf6-si8um(3g'
+SECRET_KEY = 'django-insecure-!xj@am!3!t#-@mr&!o_f9c9#5+cn_rx=a=ky@=1$#iy$a7iuk2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'videoembedding',
 ]
 
 MIDDLEWARE = [
